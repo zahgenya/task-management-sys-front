bc4fe5f8dba2a7cd85eb2302a3ddb0c4322de69c
zahgenya <zahgenya@gmail.com>
2024-01-11 14:53:07 +0200

    added: cors, env

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World')
+})
 
 app.get('/ping', (_req, res) => {
   console.log('pinged here');
