2aea92a3befd691b81019327c47280c1bf871c79
zahgenya <zahgenya@gmail.com>
2024-02-07 12:23:59 +0200

    fixed error handling, modified switch case

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import Pino from 'pino-http';
 import favicon from 'express-favicon';
@@ -41,76 +41,89 @@ interface TaskReq {
   status: status;
 }
 
-app.post('/', async (req, res) => {
+app.post('/', async (req, res, next) => {
   const { title, description } = req.body as TaskReq;
   try {
     const result = await createTask(title, description, status.toDo);
     return res.status(201).json({ result });
   } catch (err) {
-    return res.status(500).json({ err });
+    return next(err);
   }
 });
 
-app.get('/tasks', async (_req, res) => {
+app.get('/tasks', async (_req, res, next) => {
   try {
     const result = await getAllTasks();
     return res.status(200).json({ result });
   } catch (err) {
-    return res.status(500).json({ err });
+    return next(err);
   }
 });
 
-app.get('/:id', async (req, res) => {
+app.get('/:id', async (req, res, next) => {
   try {
     const taskId = req.params.id;
     const result = await getTaskById(taskId);
     return res.status(200).json({ result });
   } catch (err) {
-    return res.status(500).json({ err });
+    return next(err);
   }
 });
 
-app.delete('/:id', async (req, res) => {
+app.delete('/:id', async (req, res, next) => {
   try {
     const taskId = req.params.id;
     await deleteTaskById(taskId);
 
     return res.status(200).json({ message: 'Succesfully deleted!' });
   } catch (err) {
-    const errorMessage = err instanceof Error ? err.message : String(err);
-    return res
-      .status(500)
-      .json({ error: 'Internal Server Error', message: errorMessage });
+    return next(err);
   }
 });
 
-app.put('/:id/:method', async (req, res) => {
-  const taskId = req.params.id;
-  const method = req.params.method;
-  let result;
+app.put('/:id/:method', async (req, res, next) => {
+  try {
+    const taskId = req.params.id;
+    const method = req.params.method;
+    let result;
 
-  switch (method) {
-    case 'toDo':
-      result = await updateTaskById(taskId, status.toDo);
-      break;
+    if (!taskId || !method) {
+      return res.status(500).json({ message: 'ERROR: Invalid paramethers' });
+    }
 
-    case 'inProgress':
-      result = await updateTaskById(taskId, status.inProgress);
-      break;
+    switch (method) {
+      case 'toDo':
+        result = await updateTaskById(taskId, status.toDo);
+        break;
 
-    case 'finished':
-      result = await updateTaskById(taskId, status.finished);
-      break;
+      case 'inProgress':
+        result = await updateTaskById(taskId, status.inProgress);
+        break;
 
-    default:
-      return res.status(500).json({ message: 'ERROR: invalid method' });
+      case 'finished':
+        result = await updateTaskById(taskId, status.finished);
+        break;
+
+      default:
+        return res.status(500).json({ message: 'ERROR: invalid method' });
+    }
+
+    if (result) {
+      return res.status(200).json({ result });
+    } else {
+      return res.status(500).json({ message: 'ERROR: db error' });
+    }
+  } catch (err) {
+    return next(err);
   }
+});
 
-  if (result) {
-    return res.status(200).json({ result });
-  } else {
-    return res.status(500).json({ message: 'ERROR: db error' });
-  }
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+
+  res
+    .status(500)
+    .json({ error: 'Internal Server Error', message: err.message });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
